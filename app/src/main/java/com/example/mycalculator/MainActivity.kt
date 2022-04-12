e58d2263cf5a7dc0d6d5a2800a1bed8a0ef81554
Soruyomi <88863806+Soruyomi@users.noreply.github.com>
2022-04-12 12:26:37 -0600

    Does this work???

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -134,4 +134,70 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    companion object {
+        private var trajectories: HashMap<String, Trajectory>? = null
+        private val trajectoriesLock = ReentrantLock()
+
+        fun generateTrajectories() {
+
+            val genTrajectoryThread = Thread {
+                if (trajectories == null) {
+                    println("INFO: Trajectories loading...")
+                    trajectoriesLock.lock()
+
+                    trajectories = HashMap<String, Trajectory>()
+
+                    val pathNames = ArrayList<String>()
+
+                    val deployDirectory = Paths.get(Filesystem.getDeployDirectory().toString(), "PathWeaver/Paths")
+                    val listOfFiles = deployDirectory.toFile().listFiles()
+
+//                    for (file in listOfFiles) {
+//                        pathNames.add("/" + file.name)
+//                        // No filter is needed for now since onl files in deploy directory are path files.
+//                    }
+
+                    for (file in listOfFiles) {
+                        // System.out.println(String.format("Adding Pathname: %s", pathName));
+                        val trajPack = TrajectoryPacket.generateTrajectoryPacket(file)
+
+                        val trajectory = TrajectoryGenerator.generateTrajectory(
+                            Pose2d(trajPack.firstX, trajPack.firstY, Rotation2d.fromDegrees(trajPack.startAngle)),
+                            trajPack.pathRead,
+                            Pose2d(trajPack.lastX, trajPack.lastY, Rotation2d.fromDegrees(trajPack.endAngle)),
+                            TrajectoryConfig(7.0, 7.0).setReversed(trajPack.reversed)
+//                            TrajectoryConfig(3.0, 3.0).setReversed(trajPack.reversed)
+//                            TrajectoryConfig(2.0, 4.0)
+                        )
+
+                        trajectories!![file.name] = trajectory
+                    }
+                    trajectoriesLock.unlock()
+                    println("INFO: Trajectories loaded")
+
+
+                }
+            }
+
+            genTrajectoryThread.isDaemon = true
+            genTrajectoryThread.start()
+        }
+
+        fun getTrajectories(): HashMap<String, Trajectory>? {
+            var curTrajectories: HashMap<String, Trajectory>? = null
+
+            if (trajectories != null) {
+                println("INFO: trajectories is not null")
+            }
+
+            if (trajectories != null && trajectoriesLock.tryLock()) {
+                curTrajectories = trajectories
+                trajectoriesLock.unlock()
+            }
+
+            return curTrajectories
+        }
+    }
+
+
 }
